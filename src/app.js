2400d612f993a521e616f8ad0d6b9de8a045cba1
Anson Tsao <atsao@goaddy.com>
2020-08-05 11:31:32 -0700

    Added typescript service

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import { ServiceBroker } from 'moleculer'
+import { test } from './test'
 
 function registerServices (broker) {
   const services = {
     ...require('./services/*.service.js'),
-    ...require('./services/*.service/index.js')
+    ...require('./services/*.service/index.js'),
+    ...require('./services/*.service.ts'),
+    ...require('./services/*.service/index.ts'),
   }
   for (const service in services) {
     const schema = services[service]
     broker.createService(schema.default || schema)
   }
+  test()
 }
 
 export default async function initApp () {
---
+++ src/services/test.service.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+export default {
+  name: 'test',
+  settings: {},
+  dependencies: [],	
+  actions: {
+    hello2() {
+			return "Hello Moleculer 2";
+		},
+  },
+  events: {},
+  methods: {},
+  created() {},
+  started() {},
+  stopped() {}
+}
